6073b691fc51cf535d289268f6155938d139d918
smit <smitjogani00@gmail.com>
2023-12-25 16:38:40 +0530

    GSAP

---
+++ script.js
@@ -0,0 +1,39 @@
+var tl = gsap.timeline()
+
+tl.from("nav", {
+    opacity: 0,
+    delay: 0.3
+})
+
+tl.from("nav h1, nav h4, nav h3 ", {
+    y: -80,
+    opacity: 0,
+    duration: 0.9,
+    stagger: 0.3
+})
+
+tl.from("#left h1", {
+    opacity: 0,
+    x: -500,
+    duration: 0.5,
+    stagger: 0.4
+})
+
+tl.from("#right img", {
+    scale: 0.5,
+    opacity: 0,
+    duration: 0.5
+})
+
+gsap.from("#page2 .box", {
+    scale: 0,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.3,
+    scrollTrigger: {
+        trigger: "#page2 .box",
+        scroller: "body",
+       //markers: true,
+        start: "top 70%"
+    }
+})
